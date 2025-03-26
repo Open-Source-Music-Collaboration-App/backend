fa7cf5f5446d73b660f33c2de81bc98bf4170705
Henry <hliu669@wisc.edu>
2025-03-26 15:53:13 -0500

    Add simple testers for git handler

--- tests/git.test.js
+++ tests/git.test.js
@@ -72,7 +72,7 @@ describe('creating', () => {
     test('multiple ableton repo concurrently', async () => {
         const max_repos = 5
         let songId;
-        
+
         const promises = []
         for (let i = 1; i < max_repos; i++) {
             songId = String(i);
@@ -91,11 +91,72 @@ describe('creating', () => {
         }
     })
 
-    afterAll(async() => {
+    afterAll(async () => {
         await clearDirectory();
     })
 })
 
+describe('using git handler', () => {
+    let songId = '1';
+    const repo_path = path.join(REPOSITORY_PATH, songId);
+    let gitHandler; // our simpleGit wrapper
+    let git; // raw simpleGit obj
+    beforeAll(async () => {
+        if (fs.existsSync(repo_path)) {
+            fs.rmdirSync(repo_path, { recursive: true });
+        }
+        fs.mkdirSync(repo_path, { recursive: true });
+        gitHandler = await createGitHandler(repo_path);
+
+        git = simpleGit(repo_path);
+    })
+
+    test('verify that repo is initialized', async () => {
+        const isRepo = await git.checkIsRepo("root");
+        expect(isRepo).toBeTruthy();
+    })
+
+    test('to make an initial commit', async () => {
+        // Set up files to stage and commit
+        const content = `Some random text`;
+        const promises = [];
+        const files = Array.from({ length: 3 }, (_, i) => `file${i}.txt`);
+        files.forEach(file => {
+            const filePath = path.join(repo_path, file);
+            promises.push(fsp.writeFile(filePath, content));
+        })
+        try {
+            await Promise.all(promises);
+        } catch (e) {
+            console.error("Failed to create files:", e);
+            throw e;
+        }
+
+        const userId = "11111111";
+        const commitMessage = "Init commit";
+        const trackChanges = null;
+
+        await gitHandler.commitAbletonUpdate(userId, commitMessage, trackChanges);
+
+        const log = await git.log();
+
+        // Expects a single commit
+        expect(log.all.length).toBe(1);
+        expect(log.latest).toBe(log.all[0]);
+
+        // Check commit metadata
+        const commitMetadata = log.latest;
+        expect(commitMetadata.message).toBe(commitMessage);
+        expect(commitMetadata.author_name).toMatch(userId);
+
+        // Check added files in commit
+        const showOutput = await git.show();
+        const regexp = /file[0-2]\.txt/g;
+        const matchedFiles = [...new Set([...showOutput.matchAll(regexp)].map(match => match[0]))]
+        expect(matchedFiles.sort()).toEqual(files.sort());
+        
+    })
+})
 
 afterAll(async () => {
     console.log('running afterAll');
